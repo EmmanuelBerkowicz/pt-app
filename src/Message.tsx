58206d905f2e2ef80c854f0fb24db4e78edd2a35
Emmanuel Berkowicz <berkowiczemmanuel@gmail.com>
2025-08-11 19:04:32 +1000

    Learning React: Remove css and install bootstrap

---
+++ src/Message.tsx
@@ -0,0 +1,8 @@
+function Message() {
+  const name = "you motherfucker";
+
+  if (name != "you motherfucker") return <h1>What the fuck {name}?</h1>;
+  return <h1>What the fuckedy fuck {name}</h1>;
+}
+
+export default Message;

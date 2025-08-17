a8e47b430840d4c3890db6a63d7dcd1a91968cc4
Emmanuel Berkowicz <berkowiczemmanuel@gmail.com>
2025-08-17 21:21:02 +1000

    Update login text visuals

--- src/pages/login/LoginVisual.tsx
+++ src/pages/login/LoginVisual.tsx
@@ -3,7 +3,15 @@ import styles from "./styles/LoginVisual.module.css";
 function LoginVisual() {
   return (
     <div className={styles.LoginVisual}>
-      <h1>This is where the login visuals, logo and prompt will go</h1>
+      <h1 className={styles.LoginVisualHeading}>Welcome to your PT app!</h1>
+      <p className={styles.LoginVisualSubheading}>
+        The webapp where you can track your exercise progress and give easy
+        access to exercise routines!
+      </p>
+      <p className={styles.LoginVisualSubSubheading}>
+        Also, I really hope this gets me a job... So if you are hiring a
+        Software engineer, hello there!
+      </p>
     </div>
   );
 }
